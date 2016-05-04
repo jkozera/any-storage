fe742e4fd0157235a6ea4df8ff63de11659d8d5b
Chris Vickery <chrisinajar@gmail.com>
2016-05-03 19:52:11 -0700

    Simplify index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var storage = null;
 var sugar = require('./api-sugar');
 
-if (require('has-local-storage')) {
-  storage = require('./just-storage');
-} else if (require('has-chrome-storage')) {
+if (require('has-chrome-storage')) {
   storage = require('./chrome-storage');
 } else {
-  console.warn('Unknown storage environment! Falling back to memory storage');
+  // just storage handles local, session, and in-memory
   storage = require('./just-storage');
 }
 
